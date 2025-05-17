332250b31cdac7d4b476b085a7b78d1a88d86700
MushcV2 <titi.c2006@gmail.com>
2025-05-17 16:59:54 -0300

    Added splash screen

--- main.cpp
+++ main.cpp
@@ -2,13 +2,22 @@
 
 #include <QApplication>
 
+#include <QSplashScreen>
+#include <QTimer>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
 
     w.centralize_window( a );
-    w.show();
+
+    QSplashScreen *splash_screen = new QSplashScreen;
+    splash_screen->setPixmap( QPixmap( ":/logo/MushcFlowLogo.png" ) );
+    splash_screen->show();
+
+    QTimer::singleShot( 2000, splash_screen, &QSplashScreen::close );
+    QTimer::singleShot( 2000, &w, &QMainWindow::show );
 
     return a.exec();
 }
